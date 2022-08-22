90a05ecc461e91b6c2e8da472aadf1a847fc38d3
loveyourcode2021 <dlee513@my.bcit.ca>
2022-08-22 09:34:53 -0700

    take out secret

--- Server/index.js
+++ Server/index.js
@@ -23,54 +23,9 @@ app.use(methodOverride((req, res) => {
 }))
 
 app.use(cookieParser())
-app.post('/addfriend', async (req, res) => {
-    const { name, status } = req.body
-    const peopleRef = db.collection('people').doc('associates')
-    const res2 = await peopleRef.set({
-        [name]: status
-    }, { merge: true })
-    // friends[name] = status
-    res.status(200).send(res2)
-})
 
 
-// app.get('/friends', async (req, res) => {
-//     const peopleRef = db.collection('people').doc('associates')
-//     const doc = await peopleRef.get()
-//     if (!doc.exists) {
-//         return res.sendStatus(400)
-//     }
 
-//     res.status(200).send(doc.data())
-// })
-
-// app.get('/friends/:name', (req, res) => {
-//     const { name } = req.params
-//     if (!name || !(name in friends)) {
-//         return res.sendStatus(404)
-//     }
-//     res.status(200).send({ [name]: friends[name] })
-// })
-
-
-// app.patch('/changestatus', async (req, res) => {
-//     const { name, newStatus } = req.body
-//     const peopleRef = db.collection('people').doc('associates')
-//     const res2 = await peopleRef.set({
-//         [name]: newStatus
-//     }, { merge: true })
-//     // friends[name] = newStatus
-//     res.status(200).send(friends)
-// })
-
-// app.delete('/friends', async (req, res) => {
-//     const { name } = req.body
-//     const peopleRef = db.collection('people').doc('associates')
-//     const res2 = await peopleRef.update({
-//         [name]: FieldValue.delete()
-//     })
-//     res.status(200).send(friends)
-// })
 
 
 app.use('/v1/api/users', userRouter)
