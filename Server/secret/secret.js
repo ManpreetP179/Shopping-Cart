400b2ccb3045ad64feb98e13253fb6067e1d65f2
loveyourcode2021 <37358715+loveyourcode2021@users.noreply.github.com>
2022-08-23 17:43:33 -0700

    Update secret.js

--- Server/secret/secret.js
+++ Server/secret/secret.js
@@ -2,16 +2,6 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDjK6r_UAt5RMfChKm-XrqQrRDpZ3WxDOQ",
-  authDomain: "shopping-cart-15c11.firebaseapp.com",
-  projectId: "shopping-cart-15c11",
-  storageBucket: "shopping-cart-15c11.appspot.com",
-  messagingSenderId: "14356686512",
-  appId: "1:14356686512:web:a682811ec7ebae8cb4bd75",
-  measurementId: "G-TG517C77W2"
-};
-
 const firebaseConfig1 = {
   apiKey: process.env.API_KEY,
   authDomain: process.env.AUTH_DOMAIN,
